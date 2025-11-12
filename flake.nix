1f7fe42d76598179baf93ad946543e56c1c116c6
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-11-12 02:19:59 +0100

    Upgrade to devlib

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
     devenv.url = "github:cachix/devenv";
     devlib.url = "github:shikanime-studio/devlib";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    git-hooks.url = "github:cachix/git-hooks.nix";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     treefmt-nix.url = "github:numtide/treefmt-nix";
   };
@@ -25,67 +26,47 @@
       devenv,
       devlib,
       flake-parts,
+      git-hooks,
       treefmt-nix,
       ...
     }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         devenv.flakeModule
+        devlib.flakeModule
+        git-hooks.flakeModule
         treefmt-nix.flakeModule
       ];
       perSystem =
         { pkgs, ... }:
         {
+          devenv.shells.default = {
+            containers = pkgs.lib.mkForce { };
+            languages.nix.enable = true;
+            cachix = {
+              enable = true;
+              push = "shikanime";
+            };
+            git-hooks.hooks = {
+              actionlint.enable = true;
+              deadnix.enable = true;
+              flake-checker.enable = true;
+            };
+            gitignore = {
+              enable = true;
+              enableDefaultTemplates =true;
+            };
+            packages = [
+              pkgs.gh
+              pkgs.sapling
+            ];
           treefmt = {
-            projectRootFile = "flake.nix";
-            enableDefaultExcludes = true;
-            programs = {
-              nixfmt.enable = true;
-              prettier.enable = true;
-              shfmt.enable = true;
-              statix.enable = true;
+            enable = true;
+            config = {
+              enableDefaultExcludes = true;
+              programs.prettier.enable = true;
             };
-            settings.global.excludes = [
-              ".devenv/*"
-              ".direnv/*"
-              "LICENSE"
-            ];
           };
-          devenv = {
-            modules = [
-              devlib.devenvModule
-            ];
-            shells.default = {
-              containers = pkgs.lib.mkForce { };
-              languages.nix.enable = true;
-              cachix = {
-                enable = true;
-                push = "shikanime";
-              };
-              git-hooks.hooks = {
-                actionlint.enable = true;
-                deadnix.enable = true;
-                flake-checker.enable = true;
-              };
-              gitignore = {
-                enable = true;
-                templates = [
-                  "repo:github/gitignore/refs/heads/main/Nix.gitignore"
-                  "repo:shikanime/gitignore/refs/heads/main/Devenv.gitignore"
-                  "tt:jetbrains+all"
-                  "tt:linux"
-                  "tt:macos"
-                  "tt:vim"
-                  "tt:visualstudiocode"
-                  "tt:windows"
-                ];
-              };
-              packages = [
-                pkgs.direnv
-                pkgs.gh
-                pkgs.sapling
-              ];
-            };
           };
         };
       systems = [
